120e5f94b17540bccc1946184652151613d2a197
Umudvar <126266744+Umudvarr@users.noreply.github.com>
2023-10-17 23:20:48 +0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,40 +1,15 @@
-import {useState} from 'react';
+import './App.css';
+
+import A from './components/A';
+import B from './components/B';
 
 function App() {
-  const [name, setName] = useState('Umudvar');
-  const [age, setAge] = useState(25);
-  const [friends, setFriends] = useState(['ELi, Veli']);
-  const [address, setAddress] = useState({title: 'Baku', zip: '1099'});
   return (
-    <div className='App'>
-      <h1>Hello {name}!</h1>
-      <h2>Age {age}!</h2>
-      <button onClick={ () => setName("Habiloglu") }>ChangeName</button>
-      <br />
-      <button onClick={ () => setAge(26) }>ChangeAge</button>
-
-      <hr />
-      <br></br>
-
-      <h2>Friends</h2>
-      {
-        friends.map((friends, key) => (
-        <div key = {key}>{friends}</div>
-        ))}
-        <br />
-      <button onClick={ () => setFriends([...friends,'Pirveli']) }>AddNewFriend</button>
-
-      <hr />
-      <br></br>
-
-      <h2>Address</h2>
-      <div>{address.title} {address.zip}</div>
-      
-      <br />
-          
-      <button onClick={ () => setAddress({title: 'Gabala', zip: '1036'}) }>ChangeAddress</button>
-
-      <hr />
+    <div className="App">
+      <header className="App-header">
+        <A />
+        <B />
+      </header>
     </div>
   );
 }
---
+++ src/components/A/index.js
@@ -0,0 +1,11 @@
+import styles from './style.module.css'
+
+function A() {
+  return (
+    <div>
+    <div className={styles.title}>A</div>
+    </div>
+  )
+}
+
+export default A
